8cf497bf814715b3cd2ca798b33755cad8222e47
Antoine DUBAN <aduban@e2r3p18.42.fr>
2017-01-20 17:23:08 +0100

    swap stuff

--- utils.c
+++ utils.c
@@ -6,7 +6,7 @@
 /*   By: aduban <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/01/19 18:21:25 by aduban            #+#    #+#             */
-/*   Updated: 2017/01/19 18:58:09 by aduban           ###   ########.fr       */
+/*   Updated: 2017/01/20 17:22:11 by aduban           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -82,7 +82,6 @@ int			get_name_size(char *name)
 	int size;
 
 	size = ft_atoi(ft_strchr(name, '/') + 1);
-	ft_printf("SIZE = %d\n", size);
 	return (size);
 }
 
@@ -90,4 +89,72 @@ uint32_t	swap(uint32_t val)
 {
 	val = ((val << 8) & 0xFF00FF00) | ((val >> 8) & 0xFF00FF);
 	return (val << 16) | (val >> 16);
+}
+
+
+int64_t		swap_endian_64(int64_t i)
+{
+	return ((i >> 0x38) & 0x00000000000000ffLL)
+		| ((i >> 0x28) & 0x000000000000ff00LL)
+		| ((i >> 0x18) & 0x0000000000ff0000LL)
+		| ((i >> 0x08) & 0x00000000ff000000LL)
+		| ((i << 0x08) & 0x000000ff00000000LL)
+		| ((i << 0x18) & 0x0000ff0000000000LL)
+		| ((i << 0x28) & 0x00ff000000000000LL)
+		| ((i << 0x38) & 0xff00000000000000LL);
+}
+
+int	set_swap(int i)
+{
+	static int s = 0;
+	if (i == 0)
+		s = 0;
+	else if (i == 1)
+		s = 1;
+	return (s);
+}
+uint32_t		swap_32(uint32_t i)
+{
+	int g;
+	g = set_swap(-1);
+	if (g == 1)
+		return (swap(i));
+	return (i);
+}
+
+int64_t		swap_64(int64_t i)
+{
+	int g;
+	g = set_swap(-1);
+	if (g == 1)
+		return (swap_endian_64(i));
+	return (i);
+}
+
+
+int	set_swap_fat(int i)
+{
+	static int s = 0;
+	if (i == 0)
+		s = 0;
+	else if (i == 1)
+		s = 1;
+	return (s);
+}
+uint32_t		swap_32_fat(uint32_t i)
+{
+	int g;
+	g = set_swap_fat(-1);
+	if (g == 1)
+		return (swap(i));
+	return (i);
+}
+
+int64_t		swap_64_fat(int64_t i)
+{
+	int g;
+	g = set_swap_fat(-1);
+	if (g == 1)
+		return (swap_endian_64(i));
+	return (i);
 }
